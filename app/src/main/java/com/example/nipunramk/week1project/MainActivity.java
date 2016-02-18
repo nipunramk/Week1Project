8691da520a149047db94101ff323ebff2b712954
Nipun Ramakrishnan <nipun.ramk@berkeley.edu>
2016-02-18 15:49:59 -0800

    Learning Basic Functionality of Android Apps

---
+++ app/src/main/java/com/example/nipunramk/week1project/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.nipunramk.week1project;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button getPhoto;
+    private Button getEmail;
+    private ImageView imageView;
+    private TextView textBox1;
+    public static final int SELECT_IMAGE = 1;
+    public static final String FIRSTINTENTKEY = "firstintentkey";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getPhoto = (Button) findViewById(R.id.button1);
+        getEmail = (Button) findViewById(R.id.button2);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textBox1 = (TextView) findViewById(R.id.hello);
+
+        final String textBox1Text = textBox1.getText().toString();
+
+        getPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(Intent.ACTION_GET_CONTENT);
+                intent1.setType("image/*");
+                startActivityForResult(intent1,SELECT_IMAGE);
+            }
+        });
+
+        getEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/html");
+                intent.putExtra(Intent.EXTRA_EMAIL, "contact@mobiledevsberkeley.org");
+
+
+                startActivity(Intent.createChooser(intent, "Send Email"));
+
+            }
+        });
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+//                intent.putExtra(FIRSTINTENTKEY, textBox1Text);
+                startActivity(intent);
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_IMAGE) {
+                Uri selectedImageUri = data.getData();
+                imageView.setImageURI(selectedImageUri);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
